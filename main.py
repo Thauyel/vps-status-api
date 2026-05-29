4c0ea6666862a69efe47535bb7407abcebd1ecbe
thauyel <atacan.k03@gmail.com>
2026-05-29 20:26:45 +0000

    add: uptime, top_cpu, top_ram process stats

--- main.py
+++ main.py
@@ -35,6 +35,37 @@ def get_system_stats() -> dict:
     disk = psutil.disk_usage('/')
     cpu_load = psutil.getloadavg() if hasattr(psutil, 'getloadavg') else [0, 0, 0]
     boot_time = psutil.boot_time()
+    import time
+    uptime_seconds = time.time() - boot_time
+
+    # Top 5 CPU processes
+    top_cpu = []
+    for proc in psutil.process_iter(['pid', 'name', 'cpu_percent']):
+        try:
+            cpu = proc.info['cpu_percent']
+            if cpu is not None and cpu > 0:
+                top_cpu.append({
+                    "name": proc.info['name'],
+                    "cpu": round(cpu, 1),
+                })
+        except (psutil.NoSuchProcess, psutil.AccessDenied):
+            pass
+    top_cpu = sorted(top_cpu, key=lambda x: x['cpu'], reverse=True)[:5]
+
+    # Top 5 RAM processes
+    top_ram = []
+    for proc in psutil.process_iter(['pid', 'name', 'memory_percent']):
+        try:
+            mp = proc.info['memory_percent']
+            if mp is not None and mp > 0:
+                top_ram.append({
+                    "name": proc.info['name'],
+                    "ram_pct": round(mp, 1),
+                })
+        except (psutil.NoSuchProcess, psutil.AccessDenied):
+            pass
+    top_ram = sorted(top_ram, key=lambda x: x['ram_pct'], reverse=True)[:5]
+
     return {
         "ram_used_gb": round(mem.used / (1024**3), 1),
         "ram_total_gb": round(mem.total / (1024**3), 1),
@@ -46,6 +77,11 @@ def get_system_stats() -> dict:
         "load_1m": round(cpu_load[0], 2),
         "load_5m": round(cpu_load[1], 2),
         "load_15m": round(cpu_load[2], 2),
+        "uptime_seconds": int(uptime_seconds),
+        "uptime_days": int(uptime_seconds // 86400),
+        "uptime_hours": int((uptime_seconds % 86400) // 3600),
+        "top_cpu": top_cpu,
+        "top_ram": top_ram,
     }
 
 
